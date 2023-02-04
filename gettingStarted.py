199ac3086d6952c62d7e587246d78b40b5068301
aib333 <123673278+aib333@users.noreply.github.com>
2023-02-04 16:33:53 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,9 +5,8 @@
 
 def welcome_assignment_answers(question):
 
-    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted " \
-                   "by a TA?":
-        answer = mTCP
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "mTCP"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -21,11 +20,9 @@ def welcome_assignment_answers(question):
         answer = "9a5631e210bcf6cf2261d93e1025901adc863378239ee1a34f2cd1772cf141f0"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DNS belongs to? - The answer should be a " \
-                     "numeric number":
+    elif question == "What layer from the TCP/IP model the protocol DNS belongs to? - The answer should be a numeric number":
         answer = 5
-    elif question == "What layer of the TCP/IP model the protocol ICMP belongs to? - The answer should be a " \
-                     "numeric number":
+    elif question == "What layer of the TCP/IP model the protocol ICMP belongs to? - The answer should be a numeric number":
         answer = 4
     else:
         answer = "This is not the answer you're looking for."
@@ -35,17 +32,16 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
-    questions = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by "
-                 "a TA?",
+    questions = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
                  "Are encoding and encryption the same? - Yes/No",
-                 "Is it possible to decrypt a message without a key?"
-                 " - Yes/No", "Is it possible to decode a message without a key? - Yes/No", "Is a hashed message"
-                 " supposed to be un-hashed? - Yes/No",
+                 "Is it possible to decrypt a message without a key? - Yes/No",
+                 "Is it possible to decode a message without a key? - Yes/No",
+                 "Is a hashed message supposed to be un-hashed? - Yes/No",
                  "What is the SHA256 hashing value the following message: 'NYU Computer Networking'- Use SHA256 hash "
-                 "generator and use the answer in your code", "Is MD5 a secured"
-                 " hashing algorithm? - Yes/No", "What layer from the TCP/IP model the protocol DNS belongs to? - The"
-                 " answer should be a numeric number", "What layer of the TCP/IP model the protocol ICMP belongs to? -"
-                 " The answer should be a numeric number"
+                 "generator and use the answer in your code",
+                 "Is MD5 a secured hashing algorithm? - Yes/No",
+                 "What layer from the TCP/IP model the protocol DNS belongs to? - The answer should be a numeric number",
+                 "What layer of the TCP/IP model the protocol ICMP belongs to? - The answer should be a numeric number"
                  ]
     for debug_question in questions:
         print(debug_question)
