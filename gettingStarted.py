684583ed9d0b5f73ee6babb27ebdaa7ce24b8340
aib333 <123673278+aib333@users.noreply.github.com>
2023-02-04 16:14:22 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -23,10 +23,10 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DNS belongs to? - The answer should be a " \
                      "numeric number":
-        answer = "4"
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol ICMP belongs to? - The answer should be a " \
                      "numeric number":
-        answer = "3"
+        answer = "4"
     else:
         answer = "This is not the answer you're looking for."
     return answer
